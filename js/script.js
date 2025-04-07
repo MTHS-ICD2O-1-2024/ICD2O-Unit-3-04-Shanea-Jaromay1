f408c5edf5f782b39aa5f2838b411a944aebc201
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-04-07 16:58:25 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Show celsius
+ */
+function convertFahrenheitToCelsius() {
+  // input
+  const fahrenheitNumber = parseFloat(document.getElementById("fahrenheit-number").value);
+
+  // process 
+  const convertFahrenheitToCelsius = ( fahrenheitNumber - 32) * (5 / 9);
+
+  // output with more controlled precision
+  document.getElementById("convert").innerHTML = + fahrenheitNumber + "°F is equivalent to " + convertFahrenheitToCelsius.toFixed(3) + "°C";
+}
